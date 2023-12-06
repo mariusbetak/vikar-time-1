1441839c408c7d16b7f9c85dea1302e5870866ea
Marius <mari1a21@mail.hansenberg.dk>
2023-12-06 08:34:48 +0100

    copy from teacher

--- Example console/Program.cs
+++ Example console/Program.cs
@@ -1,11 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World! ");
+﻿﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("2Z er den bedste klasse nogensinde");
+
+/*Datatypes: int, float, string, bool
+int are whole numbers (-5, 6 , 57894132)
+float decimal numbers
+string is Text in ""
+*/
+
+// int x; //Declared x
+// x = 4; //Instansiated x
+
+// int y = 5, z = 6; //Declared and instansiated y and z
+
+// Console.WriteLine("x is " + x + " y is " + y + " z is " + z);
+
+// float d = 5.5f;
+
+// Console.WriteLine(d + x - z / y * d % x);
+
+// int e = Convert.ToInt16(d);
+
+// Console.WriteLine("float to int " + e);
 
 
-int x = 10 , y = 20 , z = 30;
+string str = "I am a string" + "\nAnd so am I";
 
+Console.WriteLine(str);
 
-
-Console.WriteLine("z is equal to " + z);
-Console.WriteLine("x is equal to " + x);
-Console.WriteLine("y is equal to " + y);
